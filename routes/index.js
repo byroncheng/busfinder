e26475ba5dd73ad835fb6abedab6bfc8c6919592
Byron Cheng <byroncheng@gmail.com>
2016-04-14 16:56:24 -0500

    Update package.json, removed some hardcoded text

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-	res.render('index', { title: 'Busfinder'});
+	res.render('index', { title: 'Busfinder', startNote: ' Try 700 or 701 (Electric Buses).'});
 });
 
 module.exports = router;
